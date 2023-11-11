b5991577a380368cf581665ab72cb21165dcc5e4
Claudia Gilabert <claugp06@gmail.com>
2023-11-11 13:30:11 +0100

    Cambio nombre URL: Se ha cambiado la url de la descarga de csv
    Se ha cambiado la direccion de descarga del csv para que sea mas fácil identificar

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
-    path('export_page/', views.CensusExportationToCSV.export_page, name='export_page'),
+    path('descargar-csv/', views.CensusExportationToCSV.export_page, name='export_page'),
     path('export-to-csv/', views.CensusExportationToCSV.export_to_csv, name='export-to-csv')
 ]
